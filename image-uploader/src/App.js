59e315e9d9d98ea80e86a2f532a20526862aa268
Mikah Pinegar <55995241+mikahpinegar@users.noreply.github.com>
2022-05-20 11:41:19 -0400

    add routes

--- image-uploader/src/App.js
+++ image-uploader/src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Container from "./components/Container/Container";
 
 function App() {
-  return <Container />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Container />}>
+          <Route index element={<Container />} />
+          <Route path="uploader" element={<Container />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

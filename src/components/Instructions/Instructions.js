80dfdbbfcaf7b331f1dfb003a6db9001b57f733d
Mikah Pinegar <55995241+mikahpinegar@users.noreply.github.com>
2023-04-26 16:04:18 -0400

    change name

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -6,7 +6,7 @@ const Instructions = () => {
     <Grid container spacing={2}>
       <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
         <Typography variant="h5">
-          Welcome to the weeds2D image uploader!
+          Welcome to the Ag Image Repo Uploader!
         </Typography>
       </Grid>
     </Grid>
